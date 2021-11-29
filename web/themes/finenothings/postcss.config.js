e0ab261ff53b2b81dd080524671b183a68bd7d5b
Brandon Rotkel <brotkel@gmail.com>
2021-11-28 19:49:55 -0800

    Various styles for article and link components.

--- web/themes/finenothings/postcss.config.js
+++ web/themes/finenothings/postcss.config.js
@@ -6,9 +6,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-nested'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [require('cssnano')]
-      : []
+    require('autoprefixer')
   ]
 };
